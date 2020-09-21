78282069fae9d2c469fc24a2177452bf9e7dc42f
P. Oscar Boykin <boykin@pobox.com>
2020-09-21 10:20:54 -1000

    commit some scripts and results

---
+++ all_table.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+for dim in 2 3 4 5 6
+do
+  #for root in 2 3 4 6 8 9 10 12 15 16 18 20 24 27 32
+  for root in 30
+  do
+    echo "dim = $dim, root = $root"
+    time ./mubs write_table --dim $dim --root $root --orth --output "tabs/orth_dim=${dim}_root=${root}"
+    time ./mubs write_table --dim $dim --root $root --unbiased --output "tabs/unbiased_dim=${dim}_root=${root}"
+  done
+done
